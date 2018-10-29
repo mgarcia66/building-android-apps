73f93ac459d5433ecde57e2f5233942e1036b466
Eric Wendelin <eric@gradle.com>
2018-10-29 15:43:43 -0700

    Provide Kotlin DSL samples

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,25 @@
 plugins {
     id("com.gradle.build-scan") version "2.0.1"
-    id("org.gradle.guides.base") version "0.15.3"
+    id("org.gradle.guides.topical") version "0.15.3"
+    id("org.gradle.guides.test-jvm-code") version "0.15.3"
 }
 
-configure<org.gradle.guides.GuidesExtension> {
+repositories {
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs")
+    }
+}
+
+dependencies {
+    constraints {
+        testImplementation("org.codehaus.groovy:groovy-all:2.4.15")
+    }
+    testImplementation("org.gradle:sample-check:0.7.0")
+    testImplementation(gradleTestKit())
+}
+
+guide {
     repoPath = "gradle-guides/building-android-apps"
-    mainAuthor = "Ken Kousen"
 }
 
 buildScan {
